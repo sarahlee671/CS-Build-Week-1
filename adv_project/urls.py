046e50631b9d59de6e2ba34d618b4ded75e89263
Sarahlee671 <sarah.lee1987@hotmail.com>
2019-09-24 08:51:15 -0700

    set up heroku

--- adv_project/urls.py
+++ adv_project/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls import include
 from rest_framework import routers
 from rest_framework.authtoken import views
 from adventure.api import PlayerViewSet, RoomViewSet
+from . import api
 
 
 # from adventure.api import move
@@ -13,9 +14,11 @@ router.register('rooms', RoomViewSet)
 router.register('currentRoom', PlayerViewSet)
 
 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    # path('api/', include(router.urls)),
     path('api/adv/', include(router.urls)),
+    path('api-token-auth', views.obtain_auth_token)
 ]
